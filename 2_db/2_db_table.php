38c7049d46d535cee90ce2ee943b48f8e6643264
szyper <zskgit@gmail.com>
2023-03-26 13:16:52 +0200

    db

--- 2_db/2_db_table.php
+++ 2_db/2_db_table.php
@@ -10,6 +10,15 @@
 </head>
 <body>
 	<h4>Użytkownicy</h4>
+  <?php
+    if (isset($_GET["userDeleteId"])){
+      if ($_GET["userDeleteId"] == 0){
+        echo "<h4>Nie usunięto użytkownika!</h4>";
+      }else{
+        echo "<h4>Usunięto użytkownika o id = $_GET[userDeleteId]</h4>";
+      }
+    }
+  ?>
   <table>
     <tr>
       <th>Imię</th>
@@ -22,10 +31,14 @@
 
 	<?php
     require_once "./scripts/connect.php";
-	  $sql = "SELECT `users`.`firstName` AS `imie` , `users`.`lastName`, `users`.`birthday`, `cities`.`city`, `states`.`state`, `countries`.`country` FROM `users` INNER JOIN `cities` ON `users`.`city_id`=`cities`.`id` INNER JOIN `states` ON `cities`.`state_id`=`states`.`id` INNER JOIN `countries` ON `states`.`country_id`=`countries`.`id`;";
+	  $sql = "SELECT `users`.`id`, `users`.`firstName` AS `imie` , `users`.`lastName`, `users`.`birthday`, `cities`.`city`, `states`.`state`, `countries`.`country` FROM `users` INNER JOIN `cities` ON `users`.`city_id`=`cities`.`id` INNER JOIN `states` ON `cities`.`state_id`=`states`.`id` INNER JOIN `countries` ON `states`.`country_id`=`countries`.`id`;";
     $result = $conn->query($sql);
-    while($user = $result->fetch_assoc()){
-      echo <<< TABLEUSERS
+//    echo $result->num_rows;
+    if ($result->num_rows == 0){
+      echo "<tr><td colspan='6'>Brak rekordów do wyświetlenia</td></tr>";
+    }else{
+	    while($user = $result->fetch_assoc()){
+		    echo <<< TABLEUSERS
         <tr>
           <td>$user[imie]</td>
           <td>$user[lastName]</td>
@@ -33,12 +46,23 @@
           <td>$user[city]</td>
           <td>$user[state]</td>
           <td>$user[country]</td>
-          <td><a href="./scripts/delete_user.php?deleteUserId=1">Usuń</a></td>
+          <td><a href="./scripts/delete_user.php?deleteUserId=$user[id]">Usuń</a></td>
         </tr>
 TABLEUSERS;
+	    }
     }
     echo "</table>";
 	?>
+    <hr>
+    <?php
+      if (isset($_GET["addUser"])){
+        echo <<<ADDUSERFORM
+          <h3>Dodaj użytkownika</h3>
+ADDUSERFORM;
+      }else{
+        echo "<a href=\"./2_db_table.php?addUser=1\">Dodaj użytkownika</a>";
+      }
+    ?>
 
 </body>
 </html>
--- 2_db/scripts/delete_user.php
+++ 2_db/scripts/delete_user.php
@@ -1,2 +1,21 @@
 <?php
-	print_r($_GET);
+//	print_r($_GET);
+//	var_dump($_GET);
+//echo $_GET["deleteUserId"];
+
+if (!empty($_GET["deleteUserId"])) {
+	require_once "./connect.php";
+//	$sql = "DELETE FROM users WHERE `users`.`firstName` = 'Anna'";
+//	$sql = "DELETE FROM users WHERE `users`.`id` = 8";
+	$sql = "DELETE FROM users WHERE `users`.`id` = $_GET[deleteUserId]";
+	$conn->query($sql);
+}
+//echo $conn->affected_rows; //1 - ok, 0 - error
+
+if ($conn->affected_rows == 0){
+	$userId = 0;
+}else{
+	$userId = $_GET["deleteUserId"];
+}
+
+header("location: ../2_db_table.php?userDeleteId=$userId");
